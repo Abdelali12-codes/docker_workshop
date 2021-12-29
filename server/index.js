fd6ea805e8160b7c667e250b6538de093eb37ef3
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-12-29 22:18:34 +0100

    env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 
+var name = process.env.NAME || "Abd"
+var city = process.env.CITY || "Casa"
 app.get('/', function (req, res) {
   res.send('Hello World Guys!');
 });
@@ -9,6 +11,14 @@ app.get('/', function (req, res) {
 
 app.get('/abdelali' , function(req , res){
     res.send("Hello abdelali")
+})
+
+app.get('/name', function(req, res){
+  res.send(`My name is ${process.env.NAME}`)
+})
+
+app.get('/city' , function(req, res){
+  res.send(`My city is ${process.env.CITY}`)
 })
 app.listen(8000, function () {
   console.log('Listening to Port 8000');
